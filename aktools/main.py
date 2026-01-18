129abb63087991775f1197ef488af60d4e7786df
Hadesong <hadesong@live.com>
2026-01-18 10:11:36 +0800

    Refactor FastAPI app initialization and routes

--- aktools/main.py
+++ aktools/main.py
@@ -27,56 +27,16 @@ favicon_path = get_favicon_path(file="favicon.ico")
 html_path = get_homepage_html(file="homepage.html")
 
 
-app = FastAPI(
-    title="欢迎访问 AKTools 为 AKShare 打造的 HTTP API 在线文档",
-    description="AKTools 是 AKShare 的 HTTP API 工具, 主要目的是使 AKShare 的数据接口部署到服务器，从而让用户通过 HTTP 访问相关接口来获取所需要的数据",
-    version=akshare.__version__,
-    redoc_url=None,
-)
+app = FastAPI()
 
 
-@app.get(
-    path="/favicon.ico", include_in_schema=False, description="获取 ico 的路径", summary="ico 的路径"
-)
-async def favicon() -> FileResponse:
-    """
-    返回自定义的 favicon
-    替换 aktools/assets/images/favicon.ico 文件则可以替换 icon
-    :return: favicon 的路径
-    :rtype: FileResponse
-    """
-    return FileResponse(favicon_path)
+@app.middleware("http")
+async def add_cache_control_header(request, call_next):
+    """ 将函数注册为 HTTP 中间件,用于拦截所有http请求,并设置缓存控制头 """
+    response = await call_next(request)
+    response.headers["Cache-Control"] = "max-age=86400" #3600一小时 86400一天
+    return response
 
-
-@app.get(path="/", tags=["主页"], description="主要展示网站首页", summary="网站首页")
-async def get_homepage(request: Request):
-    return templates.TemplateResponse(
-        "homepage.html",  # 此处的 homepage.html 可以自定义，主要采用 jinja2 模版
-        context={
-            "request": request,
-            "ip_address": request.headers["host"],
-            "ak_current_version": akshare.__version__,
-            "at_current_version": aktools.__version__,
-            "ak_latest_version": get_latest_version("akshare"),
-            "at_latest_version": get_latest_version("aktools"),
-        },
-    )
-
-
-@app.get(
-    path="/version",
-    tags=["版本"],
-    description="获取 AKTools 和 AKShare 的版本",
-    summary="获取开源库版本",
-    response_model=VersionBase,
-)
-async def get_version():
-    return {
-        "ak_current_version": akshare.__version__,
-        "at_current_version": aktools.__version__,
-        "ak_latest_version": get_latest_version("akshare"),
-        "at_latest_version": get_latest_version("aktools"),
-    }
 
 
 origins = ["*"]  # 此处设置可以访问的协议，IP和端口信息
@@ -89,7 +49,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(app_core, prefix="/api", tags=["数据接口"])
+app.include_router(app_core, prefix="/aaappi", tags=["数据接口"])
 app.include_router(app_user_login, prefix="/auth", tags=["登录接口"])
 
 if __name__ == "__main__":
